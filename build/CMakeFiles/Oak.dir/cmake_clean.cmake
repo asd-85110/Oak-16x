cdecbb1f15d0dcc2004bd11800f66953f6350420
melon <1703115048@qq.com>
2025-12-27 14:26:05 +0800

    Makefile

---
+++ build/CMakeFiles/Oak.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Oak.dir/main.cpp.obj"
+  "CMakeFiles/Oak.dir/main.cpp.obj.d"
+  "CMakeFiles/Oak.dir/res.rc.obj"
+  "D:/projects/Oak-16x/Release/Oak.exe"
+  "D:/projects/Oak-16x/Release/Oak.exe.manifest"
+  "D:/projects/Oak-16x/Release/Oak.pdb"
+  "D:/projects/Oak-16x/Release/libOak.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX RC)
+  include(CMakeFiles/Oak.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/Oak.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for Oak.
+# This may be replaced when dependencies are built.
